ec0c862e0b8e3a2679922ef519484423abebbe6e
Tommy Wells <232238585+Tommy-TheDev@users.noreply.github.com>
2026-03-11 16:55:43 -0500

    adding new model for images

---
+++ src/SimplyTammi/Models/GalleryImage.cs
@@ -0,0 +1,14 @@
+﻿namespace SimplyTammi.Models
+{
+    public class GalleryImage
+    {
+        public string Id { get; set; } = "";
+        public string Title { get; set; } = "";
+        public string Category { get; set; } = "";
+        public string ThumbUrl { get; set; } = "";
+        public string MediumUrl { get; set; } = "";
+        public string OriginalUrl { get; set; } = "";
+        public int Width { get; set; }
+        public int Height { get; set; }
+    }
+}
